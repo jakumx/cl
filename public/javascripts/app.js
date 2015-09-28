eb22ba8eb3afa6dda0cb0f33b2242bae59c67a50
josuemx <artu_ja@hotmail.com>
2015-09-28 17:58:29 -0500

    changes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
 
   var board;
   var pice = {
-    form: 'A',
+    form: 2,
     position: {
         row: 1,
-        columns: [6,7]
+        columns: [1,2]
     }
     
   } 
@@ -31,34 +31,51 @@ $(document).ready(function () {
     switch(move) {
       case 'up':
         positionRow += 1;
+        upDown();
         break;
       case 'down':
         positionRow -= 1;
+        upDown();
         break;
       case 'left':
         //console.log(positionColumns);
+        leftRight();
         break;
       case 'right':
-        //console.log(positionColumns);
+       // console.log(positionColumns);
+       leftRight();
         break;
       default:
         positionRow;
         positionColumns;
     }
-    // console.log(positionRow, pice.position.row);
-    if (positionRow > 0 && positionRow < 9 && pice.form == 'A') {
-      $('tr').find('td[data-row='+pice.position.row+'][data-column='+pice.position.columns[0]+']').css('text-decoration', 'none');
-      $('tr').find('td[data-row='+pice.position.row+'][data-column='+pice.position.columns[1]+']').css('text-decoration', 'none'); 
-      pice.position.row = positionRow;
-      $('tr').find('td[data-row='+pice.position.row+'][data-column='+pice.position.columns[0]+']').css('text-decoration', 'underline');
-      $('tr').find('td[data-row='+pice.position.row+'][data-column='+pice.position.columns[1]+']').css('text-decoration', 'underline'); 
-    }
-    if (positionColumns.length == 2) {
-      console.log(pice.position.columns);
-      pice.position.columns = [1];
-      console.log(pice.position.columns);
 
-    } 
+    function upDown () {
+      if (positionRow > 0 && positionRow < 9 && pice.form == 'A') {
+        $('tr').find('td[data-row='+pice.position.row+'][data-column='+pice.position.columns[0]+']').css('text-decoration', 'none');
+        $('tr').find('td[data-row='+pice.position.row+'][data-column='+pice.position.columns[1]+']').css('text-decoration', 'none'); 
+        pice.position.row = positionRow;
+        $('tr').find('td[data-row='+pice.position.row+'][data-column='+pice.position.columns[0]+']').css('text-decoration', 'underline');
+        $('tr').find('td[data-row='+pice.position.row+'][data-column='+pice.position.columns[1]+']').css('text-decoration', 'underline'); 
+      }
+    }
+    function leftRight () {
+      console.log(pice.form, 'before');
+      if (pice.form == 2) {
+
+        pice.form = 1;
+      } else {
+        pice.form = 2;
+      }
+      console.log(pice.form, 'after');
+    }
+    // console.log(positionRow, pice.position.row);
+    // if (positionColumns.length == 2) {
+    //   console.log(pice.position.columns);
+    //   pice.position.columns = [1];
+    //   console.log(pice.position.columns);
+
+    // } 
     // if (positionRow)
     // console.log(positionRow, pice.position.row);
     // switch(move) {
